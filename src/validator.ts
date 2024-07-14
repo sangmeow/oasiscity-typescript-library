1cdf2be0f38561fe0d8267fa995dcefd8aabdbe0
sangmeow <sickdog.oasiscity@gmail.com>
2024-07-14 12:13:22 +0900

    2024.07.14

---
+++ src/validator.ts
@@ -0,0 +1,22 @@
+import { Json } from './interface/Json.interface';
+
+export const isObjectEmpty = (data: Json, key: string): boolean => {
+  if (!data.hasOwnProperty(key)) return true;
+  return isEmpty(data[key])
+};
+
+export const isEmpty = (value: any): boolean => {
+  if (value === null || value === undefined) return true;
+  if (typeof value === 'string' && value.trim() === '') return true;
+  if (Array.isArray(value) && value.length === 0) return true;
+  if (typeof value === 'object' && Object.keys(value).length === 0) return true;
+  return false;
+}
+
+export const isNotEmpty = (value: any): boolean => {
+  if (value === null || value === undefined) return false;
+  if (typeof value === 'string' && value.trim() === '') return false;
+  if (Array.isArray(value) && value.length === 0) return false;
+  if (typeof value === 'object' && Object.keys(value).length === 0) return false;
+  return true;
+}
