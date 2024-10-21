7226172db89d10ca72bbb389703fd1f442f49fa1
sangmeow <86143088+sangmeow@users.noreply.github.com>
2024-10-21 13:47:53 +0900

    2024.10.21

--- src/libs/utcDatetime.ts
+++ src/libs/utcDatetime.ts
@@ -78,30 +78,6 @@ const validateISODateFormat = (dateString: string): boolean => {
 };
 
 /**
- * Get numeric characters from string
- * @param inputString
- * e.g. 'a1b2c3d4' -> '1234'
- */
-const getNumericCharacters = (inputString: string): string => {
-	return inputString.replace(/\D/g, "");
-};
-
-/**
- * Bind string
- * @param srcString
- * @param replaceString
- * e.g. input "19700101232221" and "20201212" results "20201212232221"
- */
-const bindString = (srcString: string, replaceString: string): string => {
-	const srcArray: string[] = srcString.split("");
-	const replacementArray: string[] = replaceString.split("");
-	for (let i = 0; i < replacementArray.length; i++) {
-		srcArray[i] = replacementArray[i];
-	}
-	return srcArray.join("");
-};
-
-/**
  * Converts 14-digit numeric string to datetime format
  * @param dateString
  * e.g. input "19700101232221" -> "1970-01-01-23:22:21"
